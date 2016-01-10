067bea24a3c47e64af9950a10f416c8fed83f9bb
Jared Haight <jared@photosandtext.com>
2016-01-10 14:36:45 -0500

    Cleaned up module.json

--- PSAttack/Modules/Module.cs
+++ PSAttack/Modules/Module.cs
@@ -3,28 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Runtime.Serialization;
 
 namespace PSAttack.Modules
 {
     public class ModuleJSON
     {
-        [JsonProperty("module")]
         public Module Module { get; set; }
     }
 
+    [DataContract]
     public class Module
     {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("url")]
-        public string URL { get; set; }
-
-        [JsonProperty("desc")]
-        public string Desc { get; set; }
-
-        [JsonProperty("keywords")]
-        public List<string> Keywords { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string url { get; set; }
     }
 }
