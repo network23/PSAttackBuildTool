4b7711bc4fe60bd7aff3fb12c7f2b67ea9f02353
Jared Haight <jared@photosandtext.com>
2016-01-10 14:37:27 -0500

    More logos!

--- PSAttack/Strings.cs
+++ PSAttack/Strings.cs
@@ -10,32 +10,126 @@ namespace PSAttack
 {
     class Strings
     {
-        public static string version = "0.1.3-alpha";
+        public static string version = "0.2.0-beta";
         public static string githubUserAgent = "PSAttack";
         public static string punchURL = "https://api.github.com/repos/jaredhaight/pspunch/releases";
-        public static string punchUnzipDir = Path.Combine(PSAUtils.GetPSAttackDir(),"PSPunch");
+        public static string punchUnzipDir = Path.Combine(PSAUtils.GetPSAttackDir(), "PSPunch");
         public static string punchZipPath = Path.Combine(PSAUtils.GetPSAttackDir(), "PSPunch.zip");
         public static string moduleSrcDir = Path.Combine(PSAUtils.GetPSAttackDir(), "ModuleSrc");
         public static string punchModulesDir = "PSPunch\\Modules\\";
         public static string punchResourcesDir = "PSPunch\\Resources\\";
         public static string punchCSProjFile = "PSPunch\\PSPunch.csproj";
         public static string punchBuildDir = Path.Combine(PSAUtils.GetPSAttackDir(), "PSPunchBuild");
-        public static string psaLogo = @"
+        public static List<string> psaLogos = new List<string>()
+        {
+@"
+  _____   _______       _______ _______       _____ _  __
+ |  __ \ / ____\ \   /\|__   __|__   __|/\   / ____| |/ /
+ | |__) | (___  \ \ /  \  | |     | |  /  \ | |    | ' / 
+ |  ___/ \___ \  > / /\ \ | |     | | / /\ \| |    |  <  
+ | |     ____) |/ / ____ \| |     | |/ ____ | |____| . \ 
+ |_|    |_____//_/_/    \_|_|     |_/_/    \_\_____|_|\_\
+",
+@"
+ (   (                                           )  
+ )\ ))\ )   (      *   ) *   )   (       (    ( /(  
+(()/(()/(   )\   ` )  /` )  /(   )\      )\   )\()) 
+ /(_)/(_)((((_)(  ( )(_)( )(_)((((_)(  (((_)|((_)\  
+(_))(_))__)\ _ )\(_(_()(_(_()) )\ _ )\ )\___|_ ((_) 
+| _ / __\ (_)_\(_|_   _|_   _| (_)_\(_((/ __| |/ /  
+|  _\__ \> / _ \   | |   | |    / _ \  | (__  ' <   
+|_| |___/_/_/ \_\  |_|   |_|   /_/ \_\  \___|_|\_\  
+",
+@"
+   ___ ____    _  _____ _____  _     ___      
+  / _ / _\ \  /_\/__   /__   \/_\   / __\/\ /\
+ / /_)\ \ \ \//_\\ / /\/ / /\//_\\ / /  / //_/
+/ ___/_\ \/ /  _  / /   / / /  _  / /__/ __ \ 
+\/    \__/_/\_/ \_\/    \/  \_/ \_\____\/  \/ 
+                                              
+",
+@"
+           __                                       
+ _____ ____\ \  _____ _____ _____ _____ _____ _____ 
+|  _  |   __\ \|  _  |_   _|_   _|  _  |     |  |  |
+|   __|__   |> |     | | |   | | |     |   --|    -|
+|__|  |_____/ /|__|__| |_|   |_| |__|__|_____|__|__|
+           /_/                                      
 
-__________  _________     /\    _____   __    __                 __    
-\______   \/   _____/    / /   /  _  \_/  |__/  |______    ____ |  | __
- |     ___/\_____  \    / /   /  /_\  \   __\   __\__  \ _/ ___\|  |/ /
- |    |    /        \  / /   /    |    \  |  |  |  / __ \\  \___|    < 
- |____|   /_______  / / /    \____|__  /__|  |__| (____  /\___  >__|_ \
-                  \/  \/             \/                \/     \/     \/
+",
+@"
+   ___  _____   ___ _______________  _______ __
+  / _ \/ __\ \ / _ /_  __/_  __/ _ |/ ___/ //_/
+ / ____\ \  > / __ |/ /   / / / __ / /__/ ,<   
+/_/  /___/ /_/_/ |_/_/   /_/ /_/ |_\___/_/|_|  
+                                               
 
-";
+",
+@"
+ ######   #####  #       #    ####### #######    #     #####  #    # 
+ #     # #     #  #     # #      #       #      # #   #     # #   #  
+ #     # #         #   #   #     #       #     #   #  #       #  #   
+ ######   #####     # #     #    #       #    #     # #       ###    
+ #             #   #  #######    #       #    ####### #       #  #   
+ #       #     #  #   #     #    #       #    #     # #     # #   #  
+ #        #####  #    #     #    #       #    #     #  #####  #    # 
+                                                                     
+",
+@"
+ ____    ____     __    ______  ______ ______ ______  ____    __  __     
+/\  _`\ /\  _`\  /\ `\ /\  _  \/\__  _/\__  _/\  _  \/\  _`\ /\ \/\ \    
+\ \ \L\ \ \,\L\_\\ `\ `\ \ \L\ \/_/\ \\/_/\ \\ \ \L\ \ \ \/\_\ \ \/'/'   
+ \ \ ,__/\/_\__ \ `\ >  \ \  __ \ \ \ \  \ \ \\ \  __ \ \ \/_/\ \ , <    
+  \ \ \/   /\ \L\ \ /  / \ \ \/\ \ \ \ \  \ \ \\ \ \/\ \ \ \L\ \ \ \\`\  
+   \ \_\   \ `\____/\_/   \ \_\ \_\ \ \_\  \ \_\\ \_\ \_\ \____/\ \_\ \_\
+    \/_/    \/_____\//     \/_/\/_/  \/_/   \/_/ \/_/\/_/\/___/  \/_/\/_/
 
+",
+@"
+  _   __       ___ ___      _    
+ |_) (_  \  /\  |   |  /\  /  |/ 
+ |   __) / /--\ |   | /--\ \_ |\ 
+                                 
+",
+@"
+ ______   ______    _______ _______ _______ _______ _______ _     _ 
+(_____ \ / _____)_ (_______(_______(_______(_______(_______(_)   | |
+ _____) ( (____ ( \ _______    _       _    _______ _       _____| |
+|  ____/ \____ \ ) |  ___  |  | |     | |  |  ___  | |     |  _   _)
+| |      _____) (_/| |   | |  | |     | |  | |   | | |_____| |  \ \ 
+|_|     (______/   |_|   |_|  |_|     |_|  |_|   |_|\______|_|   \_)
+                                                                    
+",
+@"
+      ___      ___         ___                          ___         ___         ___     
+     /  /\    /  /\       /  /\        ___      ___    /  /\       /  /\       /__/|    
+    /  /::\  /  /:/_     /  /::\      /  /\    /  /\  /  /::\     /  /:/      |  |:|    
+   /  /:/\:\/  /:/ /\   /  /:/\:\    /  /:/   /  /:/ /  /:/\:\   /  /:/       |  |:|    
+  /  /:/~/:/  /:/ /::\ /  /:/~/::\  /  /:/   /  /:/ /  /:/~/::\ /  /:/  ___ __|  |:|    
+ /__/:/ /:/__/:/ /:/\:/__/:/ /:/\:\/  /::\  /  /::\/__/:/ /:/\:/__/:/  /  //__/\_|:|____
+ \  \:\/:/\  \:\/:/~/:\  \:\/:/__\/__/:/\:\/__/:/\:\  \:\/:/__\\  \:\ /  /:\  \:\/:::::/
+  \  \::/  \  \::/ /:/ \  \::/    \__\/  \:\__\/  \:\  \::/     \  \:\  /:/ \  \::/~~~~ 
+   \  \:\   \__\/ /:/   \  \:\         \  \:\   \  \:\  \:\      \  \:\/:/   \  \:\     
+    \  \:\    /__/:/     \  \:\         \__\/    \__\/\  \:\      \  \::/     \  \:\    
+     \__\/    \__\/       \__\/                        \__\/       \__\/       \__\/    
+
+",
+@"
+   _ \    ___| \ \     \     |    |                 |     | 
+  |   | \___ \  \ \   _ \    __|  __|   _` |   __|  |  /  | 
+  ___/        |   /  ___ \   |    |    (   |  (       <  _| 
+ _|     _____/  _/ _/    _\ \__| \__| \__,_| \___| _|\_\ _) 
+                                                            
+"
+        };
         public static string psaWarning = @"
  ############################################################
  #                                                          #
- #    PLEASE NOTE: This is an alpha release of PS>Attack    #
- # There are plenty of bugs and not a lot of functionality. # 
+ #     PLEASE NOTE: This is a beta release of PS>Attack     #
+ #   Things might be buggy. If you find something that's    #
+ #             broken please submit an issue at             #
+ #      https://github.com/jaredhaight/psattack/issues      #
+ #        or even better, submit a pull request! :-D        #
  #                                                          #
  #         For more info view the release notes at          #
  #   https://www.github.com/jaredhaight/psattack/releases   #
@@ -72,7 +166,7 @@ __________  _________     /\    _____   __    __                 __
 
  Press return to open up the folder. Thanks for using PS>Attack!
 ";
-    public static string psaEndNoMSBuild = @"
+        public static string psaEndNoMSBuild = @"
  Hrm.. we couldn't find MSBuild.exe. That _should_ be here:
 
  {0}
@@ -83,7 +177,7 @@ __________  _________     /\    _____   __    __                 __
 
  Press return to close this window.
  ";
-    public static string psaEndFailure = @"
+        public static string psaEndFailure = @"
  Oh no! It looks like the build failed. You should check the build
  output above and see if there's an obvious issue. If you can't 
  resolve the problem on your own, go ahead and submit an issue  at
@@ -92,5 +186,7 @@ __________  _________     /\    _____   __    __                 __
  gray text after '[*] Building PSPunch..' upto this error message)
 
  Press return to close this window.
- ";  }
+ ";
+    }
 }
+
